b0f7a7a1b39766ec46f5d274a4e0722cbe8116d0
tonyzhao6 <tony.zhao@idinsight.org>
2024-10-10 07:17:47 -0400

    Adding in gibberish model functionality. (#10)
    * Adding in gibberish model functionality.
    
    * Loading gibberish model at top-level once instead of repeated loading each time search endpoint is invoked.
    
    ---------
    
    Co-authored-by: tonyzhao6 <>

--- core_backend/app/question_answer/routers.py
+++ core_backend/app/question_answer/routers.py
@@ -3,10 +3,13 @@ endpoints.
 """
 
 import os
+import pickle
 from io import BytesIO
+from pathlib import Path
 from typing import Tuple
 
 from fastapi import APIRouter, Depends, status
+from fastapi.exceptions import HTTPException
 from fastapi.requests import Request
 from fastapi.responses import JSONResponse
 from sqlalchemy.exc import IntegrityError
@@ -55,6 +58,7 @@ from .schemas import (
     ResponseFeedbackBase,
 )
 from .speech_components.external_voice_components import transcribe_audio
+from .utils import is_gibberish
 
 logger = setup_logger()
 
@@ -70,6 +74,8 @@ router = APIRouter(
     dependencies=[Depends(authenticate_key), Depends(rate_limiter)],
     tags=[TAG_METADATA["name"]],
 )
+
+gibberish_model = pickle.load(open(Path(__file__).parent / "gibberish_model.pkl", "rb"))
 
 
 @router.post(
@@ -88,12 +94,33 @@ async def search(
     asession: AsyncSession = Depends(get_async_session),
     user_db: UserDB = Depends(authenticate_key),
 ) -> QueryResponse | JSONResponse:
-    """
-    Search endpoint finds the most similar content to the user query and optionally
+    """Search endpoint finds the most similar content to the user query and optionally
     generates a single-turn LLM response.
 
     If any guardrails fail, the embeddings search is still done and an error 400 is
     returned that includes the search results as well as the details of the failure.
+
+    Parameters
+    ----------
+    user_query
+        QueryBase object containing the user query and metadata.
+    request
+        FastAPI request object.
+    asession
+        AsyncSession object for database transactions.
+    user_db
+        UserDB object containing the user's metadata.
+
+    Returns
+    -------
+    QueryResponse | JSONResponse
+        QueryResponse object containing the search results and LLM response, or a
+        JSONResponse object containing an error message.
+
+    Raises
+    ------
+    HTTPException
+        If gibberish text is detected in the user query.
     """
 
     (
@@ -106,6 +133,14 @@ async def search(
         asession=asession,
         generate_tts=False,
     )
+    if is_gibberish(
+        gibberish_model=gibberish_model, text=user_query_refined_template.query_text
+    ):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=f"Gibberish text detected: {user_query_refined_template.query_text}",
+        )
+
     response = await get_search_response(
         query_refined=user_query_refined_template,
         response=response_template,
